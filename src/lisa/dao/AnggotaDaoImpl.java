bd9958f8c3bafd28ef53c776e12574f41b2b3834
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2023-05-03 22:03:39 +0700

    Update AnggotaDaoImpl.java

--- src/lisa/dao/AnggotaDaoImpl.java
+++ src/lisa/dao/AnggotaDaoImpl.java
@@ -5,9 +5,13 @@
  */
 package lisa.dao;
 
-import com.mysql.jdbc.Connection;
+import java.sql.Connection;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
 import lisa.model.Anggota;
+
 
 /**
  *
@@ -16,7 +20,7 @@ import lisa.model.Anggota;
 public class AnggotaDaoImpl implements AnggotaDao{
 
     @Override
-    public void insert(java.sql.Connection con, Anggota anggota) throws Exception{
+    public void insert(Connection con, Anggota anggota) throws Exception {
        String sql = "Insert into anggota values(?,?,?,?)";
        PreparedStatement ps = con.prepareStatement(sql);
        ps.setString(1, anggota.getKodeanggota());
@@ -25,5 +29,62 @@ public class AnggotaDaoImpl implements AnggotaDao{
        ps.setString(4,anggota.getJeniskelamin());
        ps.executeUpdate();
     }
-    
+
+    @Override
+    public void update(Connection con, Anggota anggota) throws Exception {
+       String sql = "Update anggota set namaanggota=?, alamat=?,"
+                     + "jeniskelamin=? where kodeanggota=?";
+       PreparedStatement ps = con.prepareStatement(sql);
+       ps.setString(1, anggota.getKodeanggota());
+       ps.setString(2,anggota.getNamaanggota());
+       ps.setString(3,anggota.getAlamat());
+       ps.setString(4,anggota.getJeniskelamin());
+       ps.executeUpdate();
+    }
+
+    @Override
+    public void delete(Connection con, Anggota anggota) throws Exception {
+       String sql = "delete from anggota"
+               + "where kodeanggota=?";
+       PreparedStatement ps = con.prepareStatement(sql);
+       ps.setString(1, anggota.getKodeanggota());
+       ps.executeUpdate();
+    }
+
+    @Override
+    public Anggota getAnggota(Connection con, String kode) throws Exception {
+       String sql = "select * from anggota"
+                    + "where kodeanggota = ?";
+       PreparedStatement ps = con.prepareStatement(sql);
+       ps.setString(1, kode);
+       ResultSet rs = ps.executeQuery();
+       Anggota anggota = null;
+       if (rs.next()){
+           anggota = new Anggota();
+           anggota.setKodeanggota(rs.getString(1));
+           anggota.setNamaanggota(rs.getString(2));
+           anggota.setAlamat(rs.getString(3));
+           anggota.setJeniskelamin(rs.getString(4));
+       }
+       return anggota;
+    }
+
+    @Override
+    public List<Anggota> getAllAnggota(Connection con) throws Exception {
+       String sql = "select * from anggota";
+       PreparedStatement ps = con.prepareStatement(sql);
+       ResultSet rs = ps.executeQuery();
+       List<Anggota> list = new ArrayList<>();
+       Anggota anggota = null;
+       while (rs.next()){
+           anggota = new Anggota();
+           anggota.setKodeanggota(rs.getString(1));
+           anggota.setNamaanggota(rs.getString(2));
+           anggota.setAlamat(rs.getString(3));
+           anggota.setJeniskelamin(rs.getString(4));
+           list.add(anggota);
+       }
+       return list;
+    }
+
 }
