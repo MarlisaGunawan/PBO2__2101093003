91378027bea2f76fbe8dc638dc1b39a0dbc759b8
MarlisaGunawan <114122692+MarlisaGunawan@users.noreply.github.com>
2023-06-14 12:41:15 +0700

    Create PBO2_2101092062.java

---
+++ src/pbo2_2101092062/PBO2_2101092062.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo2_2101092062;
+import lisa.Dao.Koneksi;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+
+
+/**
+ *
+ * @author HP Folio 1040
+ */
+public class PBO2_2101092062 {
+
+    /**
+     * @param args the command line arguments
+     */
+
+    public static void main(String[] args) throws SQLException {
+        try {
+             Koneksi koneksi = new Koneksi ();
+            Connection con = koneksi.getKoneksi();
+            JOptionPane.showMessageDialog(null, "Koneksi Ok");
+        } catch (Exception ex) {
+            JOptionPane.showMessageDialog(null, "Error : " + ex.getMessage());
+            Logger.getLogger(PBO2_2101092062.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+}
